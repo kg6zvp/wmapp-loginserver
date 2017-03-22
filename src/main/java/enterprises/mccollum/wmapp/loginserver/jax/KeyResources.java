847a2ac3fa0cc0384cf9e3ea65fb64138c1c36c4
Sam McCollum <smccollum@mccollum.enterprises>
2017-03-22 05:50:43 -0700

    Finished and tested PEM encoding for public key API endpoint

--- src/main/java/enterprises/mccollum/wmapp/loginserver/jax/KeyResources.java
+++ src/main/java/enterprises/mccollum/wmapp/loginserver/jax/KeyResources.java
@@ -14,9 +14,35 @@ public class KeyResources {
 	@Inject
 	CryptoSingleton cs;
 	
+	private final static String BEGIN_PUBKEY_PADDING = "-----BEGIN PUBLIC KEY-----";
+	private final static String END_PUBKEY_PADDING = "-----END PUBLIC KEY-----";
+	
 	@GET
 	@Path("getPubKey")
 	public Response getPublicKey(){
-		return Response.ok(Base64.encode(cs.getPublicKey().getEncoded())).build();
+		return Response.ok(getPemString(Base64.encode(cs.getPublicKey().getEncoded()))).build();
+	}
+	
+	private String getPemString(String b64String){
+		return BEGIN_PUBKEY_PADDING+'\n'+
+				addReturns(b64String)+'\n'+
+				END_PUBKEY_PADDING+'\n';
+	}
+
+	private String addReturns(String b64String) {
+		StringBuilder sb = new StringBuilder(b64String.length()+20);
+		int end = 0;
+		int i = 0;
+		for(i = 0; i < b64String.length(); i += 64){
+			end = i+64;
+			System.out.printf("begin: %d\nend: %d\n", i, end);
+			if(end < b64String.length()){
+				sb.append(b64String.substring(i, end)+"\n");
+			}
+		}
+		sb.append(b64String.substring(i-64));
+		System.out.println("Built string: "+sb.toString());
+		return sb.toString();
+		//return b64String.replaceAll("(.{64})", "\n");
 	}
 }
