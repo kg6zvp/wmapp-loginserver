9de1ddc005a64c3cf5ad3a0a5e6c4a12a0eaa348
Sam McCollum <smccollum@mccollum.enterprises>
2017-03-26 15:22:04 -0700

    Fix autodoc scripts to allow for

--- autodoc.sh
+++ autodoc.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-apidoc -i ./ -o apidocs/
 
+function clean(){
+	rm -rf apidocs
+}
+
+function make(){
+	clean
+	apidoc -i ./ -o apidocs/
+}
+
+if [ $# -eq 0 ]; then
+	echo "clean and make"
+	make
+else
+	if [[ "$1" == "help" ]]; then
+		echo "Run without arguments to clean, then make docs"
+		echo "Run with the argument either 'clean' or 'make' in order to specify the operation to be performed"
+		exit
+	fi
+	echo "running script"
+	$1
+fi
